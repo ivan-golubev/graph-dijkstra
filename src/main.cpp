15653d918dbee184547cd2ce507eee478507adb3
Ivan Golubev <ivan.x64@protonmail.com>
2025-03-18 13:13:12 +0000

    Dijsktra implemented

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <format>
 
 struct Edge {
+	int from;
 	int to;
 	int weight;
 };	
@@ -20,8 +21,8 @@ public:
 		--from; --to; // 0 indexed
 		assert(from >= 0 && from < VertexCount);
 		assert(to >= 0 && to < VertexCount);
-		AdjList[from].emplace_back(to, weight);
-		AdjList[to].emplace_back(from, weight);
+		AdjList[from].emplace_back(from, to, weight);
+		AdjList[to].emplace_back(to, from, weight);
 	}	
 };
 
@@ -31,21 +32,39 @@ std::array<int, VertexCount> FindShortestPaths(Graph<VertexCount>& graph, int so
 	assert(source >= 0 && source < VertexCount);
 	std::array<int, VertexCount> shortestPaths;
 	shortestPaths.fill(std::numeric_limits<int>::max()); // distance to all vertices is unknown
-
+	shortestPaths[source] = 0; // path to the source itself
 	// Dijkstra's algorithm
 	auto comparator = [](const Edge& left, const Edge& right) {
 		return left.weight > right.weight;
 	};
 	std::priority_queue<Edge, std::vector<Edge>, decltype(comparator)> minHeap;
-	for (const auto& edge: graph.AdjList[0])
+	for (const auto& edge: graph.AdjList[source])
 		minHeap.push(edge);
+	std::array<bool,VertexCount> visitedVertices = {};
 	while (!minHeap.empty()) 
 	{
-		Edge shortest = minHeap.top(); minHeap.pop();
-		std::cout << std::format("Edge from {} to {} with weight = {}\n", source+1, shortest.to+1, shortest.weight);
-
+		Edge edge = minHeap.top(); minHeap.pop();
+		shortestPaths[edge.to] = std::min(shortestPaths[edge.to], shortestPaths[edge.from]+edge.weight);
+#ifndef NDEBUG
+		std::cout << std::format("Edge from {} to {} with weight = {}\n", source+1, edge.to+1, shortestPaths[edge.to]);
+#endif
+		visitedVertices[edge.from] = true;
+		// push the edges from this vertex
+		for (const auto& edge: graph.AdjList[edge.to]) {
+			if (!visitedVertices[edge.to])
+				minHeap.push(edge);
+		}
 	}
 	return shortestPaths;
+}
+
+template<std::size_t VertexCount>
+void printPaths(int source, const std::array<int, VertexCount>& paths) {
+	std::cout << "Shortest paths:\n";
+	for (auto i=0; i<paths.size(); ++i) {
+		if (paths[i] < std::numeric_limits<int>::max())
+			std::cout << std::format("{}->{} = {}\n", source, i+1, paths[i]);
+	}
 }
 
 int main() {
@@ -63,6 +82,8 @@ int main() {
 
 	graph.AddEdge(4,5,6);
 	graph.AddEdge(5,6,9);
-	auto shortestPaths = FindShortestPaths(graph, 1);
+	constexpr int from = 1;
+	auto shortestPaths = FindShortestPaths(graph, from);
+	printPaths(from, shortestPaths);
 	return 0;
 }
